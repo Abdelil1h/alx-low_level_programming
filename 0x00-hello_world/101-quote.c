cc9dad09799864f591ad57a7f5eb128ce565d44a
Abdelil1h <abdeilahbou2017@gmail.com>
2023-07-13 16:38:26 -0700

    hi

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-    ssize_t len = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n") - 1;
-    ssize_t bytes_written = write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", len);
+ssize_t len = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n") - 1;
+ssize_t bytes_written = write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", len);
 
-    if (bytes_written != len)
-        return (1);
+if (bytes_written != len)
+return (1);
 
-    return (1);
+return (1);
 }
