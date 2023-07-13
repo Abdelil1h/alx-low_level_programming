f519e168c7c30702e396c9e52431ce41c012022f
Abdelil1h <abdeilahbou2017@gmail.com>
2023-07-13 07:08:53 -0700

    hi

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,13 +4,14 @@
  * main - Entry point
  *
  * Description: Prints "with proper grammar, but the outcome is a piece of art,"
- *              
+ *              followed by a new line.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+    printf("with proper grammar, but the outcome is a piece of art,"
+           "\n");
 
-	return (0);
+    return 0;
 }
