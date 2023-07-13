496d3ba6ab135f2b2e613dab8c5ce23706fc5035
Abdelil1h <abdeilahbou2017@gmail.com>
2023-07-13 16:54:26 -0700

    hi

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,16 +2,13 @@
 
 /**
  * main - Entry point
- *
- * Return: Always 1 (Error)
+ * followed by a newline, to standaed error.
+ * Return: Always 1.
  */
+
+
 int main(void)
 {
-    ssize_t len = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n") - 1;
-    ssize_t bytes_written = write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", len);
-
-    if (bytes_written != len)
-        return 1;
-
-    return 1;
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+        return (1);
 }
