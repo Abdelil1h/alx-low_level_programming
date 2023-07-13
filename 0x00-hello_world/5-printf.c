8495039a8c0b06c4e0ecefa87a57d1d83ee86efd
Abdelil1h <abdeilahbou2017@gmail.com>
2023-07-13 07:02:39 -0700

    hi

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  * main - Entry point
  *
  * Description: Prints "with proper grammar, but the outcome is a piece of art,"
- *              followed by a new line.
+ *              
  *
  * Return: Always 0 (Success)
  */
