62c5539f5e86937fb66bee70b91eebffb613ee45
Abdelil1h <abdeilahbou2017@gmail.com>
2023-07-13 14:09:19 -0700

    hi

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
- *
- * Description: Prints "with proper grammar, but the outcome is a piece of art,"
- *              followed by a new line.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-
+	printf("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
