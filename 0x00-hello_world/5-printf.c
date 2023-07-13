37ca41cd19e3e9e104a05bb08a2fc1ae87fbf9ac
Abdelil1h <abdeilahbou2017@gmail.com>
2023-07-13 15:03:43 -0700

    hi

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,5 +7,7 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art ,");
+        fprintf(stderr, "(56 chars long)\n");
+        fprintf(stderr, "[stderr]: [Anything]\n");
 	return (0);
 }
